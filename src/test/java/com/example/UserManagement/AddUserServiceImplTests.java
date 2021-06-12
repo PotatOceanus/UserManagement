2072775d28590b39a5af8cf4cef2a28d0fd3d97e
PotatOceanus <39735195+PotatOceanus@users.noreply.github.com>
2021-06-12 23:35:02 +1000

    Merge remote-tracking branch 'origin/master'

---
+++ src/test/java/com/example/UserManagement/AddUserServiceImplTests.java
@@ -0,0 +1,12 @@
+package com.example.UserManagement;
+
+import org.mockito.Spy;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class AddUserServiceImplTests {
+
+    @Spy
+
+
+}
